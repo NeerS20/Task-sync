5afb7f1d5760f597a357603715665ee9e59a24f0
Neeraj <guptaneer01@gmail.com>
2024-06-01 18:00:38 +0530

    init

---
+++ src/main/java/com/h_a_r_r_y/Task_sync/TaskSyncApplication.java
@@ -0,0 +1,13 @@
+package com.h_a_r_r_y.Task_sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/h_a_r_r_y/Task_sync/TaskSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h_a_r_r_y.Task_sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
